22ddca465fe446548612755c21ec5e9465c3e4df
PC <onure7@icloud.com>
2025-01-09 13:35:44 +0300

    firebase initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:neon_academy_workspace/authentication/login/view/login_view.dart';
 import 'package:neon_academy_workspace/bloc/bloc_view.dart';
 import 'package:neon_academy_workspace/bloc/counter_bloc.dart';
+import 'package:neon_academy_workspace/firebase_options.dart';
 import 'package:neon_academy_workspace/lottie.dart';
 import 'package:neon_academy_workspace/stepper.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MainApp(),
   );
